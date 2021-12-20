c61c47319fcb1c610998c6f596fd390f0329fcf2
Flavio31fdf <flavio13fdf@gmail.com>
2021-12-20 05:41:49 +0100

    2do cambios vista
    mejoras de vistas

--- blog/apps/posts/urls.py
+++ blog/apps/posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import DetallePosts
 
 urlpatterns = [
     path('', views.ListarPosts.as_view(), name='listar'),
+    path('<slug:pk>/detalle', views.DetallePosts.as_view(), name='detalle'),
 ] 
--- blog/apps/posts/views.py
+++ blog/apps/posts/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Post
 
 class ListarPosts(ListView):	
 	model= Post
-	template_name = "post_list.html"
+	template_name = "posts/post_list.html"
 	context_object_name = "posts"
     
 	""" def get_queryset(self):
 		noticias = Noticia.objects.all().order_by('-fecha_creacion')
 		return noticias """
+class DetallePosts(DetailView):
+	model=Post
+	template_name="posts/post_detail.html"		
